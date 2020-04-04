31b74eab8550d6bde8dd344c6773ce9f203c4a9d
Adam <AdamAucamp@users.noreply.github.com>
2020-04-04 12:07:29 +0200

    add firebase and redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useFirestoreDocData, useFirestore, SuspenseWithPerf} from 'reactfire';
+
+import { Counter } from './features/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+
+       {/*
+        SuspenseWithPerf behaves the same as Suspense,
+        but also automatically measures load times with the User Timing API
+        and reports it to Firebase Performance Monitoring
+      */}
+      <SuspenseWithPerf
+        fallback={'loading burrito status...'}
+        traceId={'load-burrito-status'}
+      >
+        <Burrito />
+
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Counter />
+        
+      </SuspenseWithPerf>
     </div>
   );
 }
 
 export default App;
+
+
+function Burrito() {
+  // lazy load the Firestore SDK and create a document reference
+  const burritoRef = useFirestore()
+    .collection('customers')
+    .doc('xYYwDvxOpXicVDTEiJdU');
+
+  // subscribe to the doc. just one line!
+  const burrito = useFirestoreDocData(burritoRef);
+
+  // get the value from the doc
+  const name = burrito.email;
+
+  return <p>The burrito is {name}</p>;
+}
